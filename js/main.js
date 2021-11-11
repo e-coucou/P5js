cb05331b5e0950ace86eff03e81e6f50a1b74319
e-Coucou <eric@e-coucou.com>
2021-11-11 23:20:56 +0100

    updt

--- js/main.js
+++ js/main.js
@@ -1,24 +1,152 @@
-var applis = [];
-var img = [];
-var jsonApp;
+$(document).ready(function() {
+  var MainNav = {};
+  var RightAside = {};
+  var ESCAPE_CODE = 27;
+  var rightActive;
 
-function preload() {
-  jsonApp = loadJSON('./data/app.json');
-  img.push(loadImage('./img/2048.png'));
-}
+  MainNav.App = (function() {
+    var navButton = $('#menu-button'),
+      navMenu = $('#global-nav');
 
-function setup() {
-  // createCanvas(400, 400);
-  noCanvas;
-  for (i in jsonApp) {
-    applis.push(jsonApp[i]);
-  }
-  for (a of applis) {
-    p = createP(a.name);
-  }
+    var navLinks = navMenu.find('a');
 
-}
+    function initApp() {
+      navMenu.on('keydown', handleKeydown);
+      navButton.on('click', handleClick);
+      disableNavLinks();
+    }
 
-// function draw() {
-//   background(0);
-// }
+    function handleKeydown(event) {
+      if (event.keyCode === ESCAPE_CODE) {
+        document.body.classList.toggle('active');
+        document.getElementById('menu-button').classList.remove('active');
+        disableNavLinks();
+        navButton.focus();
+      }
+    }
+
+    function handleClick(event) {
+      if (document.body.classList.contains('active')) {
+        document.body.classList.remove('active');
+        document.getElementById('menu-button').classList.remove('active');
+        disableNavLinks();
+      } else {
+        document.body.classList.add('active');
+        document.getElementById('menu-button').classList.add('active');
+        enableNavLinks();
+        navLinks.eq(0).focus();
+      }
+    }
+
+    function enableNavLinks() {
+      navButton.attr('aria-label', 'Close navigation menu');
+      navButton.attr('aria-expanded', 'true');
+      navMenu.removeAttr('aria-hidden');
+      navLinks.removeAttr('tabIndex');
+    }
+
+    function disableNavLinks() {
+      navButton.attr('aria-label', 'Open navigation menu');
+      navButton.attr('aria-expanded', 'false');
+      navMenu.attr('aria-hidden', 'true');
+      navLinks.attr('tabIndex', '-1');
+    }
+    return {
+      init: function() {
+        initApp();
+      }
+    }
+  })();
+
+  RightAside.App = (function() {
+    var showText, hideText;
+    switch (window.location.pathname) {
+      case '/learning/':
+        showText = 'Show courses links';
+        hideText = 'Hide courses links';
+        break;
+      case '/books/':
+        showText = 'View books';
+        hideText = 'Hide books';
+        break;
+      case '/videos/':
+        showText = 'Video quick links';
+        hideText = 'Hide quick links';
+        break;
+      default:
+        showText = 'Show quick links';
+        hideText = 'Hide quick links';
+    }
+    var quicklinksButton = $('#quicklinks-btn');
+    var quicklinksMenu = $('#quicklinks-section');
+    var qlLinks = quicklinksMenu.find('a');
+
+    function initApp() {
+      quicklinksMenu.on('keydown', handleKeydown);
+      quicklinksButton.on('click', handleClick);
+
+      if ($(document).width() > 700) {
+        rightActive = true;
+        enableQuicklinks();
+        $('.nav-links a, .nav-logo').click(function(e) {
+          e.preventDefault();
+          var url = $(this).attr("href");
+          $('.left-container, .right-container').addClass('out');
+          $('nav').addClass('moving');
+          setTimeout(function() {
+            window.location = url;
+          }, 600);
+        });
+      } else {
+        rightActive = false;
+        disableQuicklinks();
+        quicklinksButton.css('display', 'block');
+      }
+    }
+
+    function handleKeydown() {
+      if (event.keyCode === ESCAPE_CODE) {
+        rightActive = false;
+        disableQuicklinks();
+        quicklinksButton.focus();
+      }
+    }
+
+    function handleClick() {
+      if (rightActive === true) {
+        rightActive = false;
+        disableQuicklinks();
+      } else {
+        rightActive = true;
+        enableQuicklinks();
+        qlLinks.eq(0).focus();
+      }
+    }
+
+    function enableQuicklinks() {
+      quicklinksButton.html(hideText);
+      $('.right-container').removeClass('out');
+      $('.right-container').addClass('in');
+      quicklinksButton.attr('aria-expanded', 'true');
+      quicklinksMenu.removeAttr('aria-hidden');
+      qlLinks.removeAttr('tabindex');
+    }
+
+    function disableQuicklinks() {
+      quicklinksButton.html(showText);
+      $('.right-container').removeClass('in');
+      $('.right-container').addClass('out');
+      quicklinksButton.attr('aria-expanded', 'false');
+      quicklinksMenu.attr('aria-hidden', 'true');
+      qlLinks.attr('tabindex', '-1');
+    }
+    return {
+      init: function() {
+        initApp();
+      }
+    }
+  })();
+  new MainNav.App.init();
+  new RightAside.App.init();
+  $('.left-container').removeClass('out');
+});
