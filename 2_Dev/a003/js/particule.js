9e9e5074d8ce4d4c4630bbbb7c87c47810eebbad
e-Coucou <eric@e-coucou.com>
2021-12-16 00:38:25 +0100

    ok

--- 2_Dev/a003/js/particule.js
+++ 2_Dev/a003/js/particule.js
@@ -1,10 +1,10 @@
 class Particule {
-    constructor(_x,_y) {
-        this.pos = createVector(_x,_y);
-        this.vel = p5.Vector.fromAngle(random(0,PI));
+    constructor(_x, _y) {
+        this.pos = createVector(_x, _y);
+        this.vel = p5.Vector.fromAngle(random(0, PI));
         // this.vel = p5.Vector.random2D();
-        this.vel.mult(random(0.5,1.5));
-        this.acc = createVector(0,0);
+        this.vel.mult(random(0.5, 1.5));
+        this.acc = createVector(0, 0);
         this.r = 1;
         this.color = 0;
         this.fixe = false;
@@ -13,18 +13,18 @@ class Particule {
 
     update() {
         if (!this.fixe) {
-        this.vel.mult(0.99);
+            this.vel.mult(0.99);
 
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
-        this.acc.mult(0);
-        this.life -= 3;
-        this.color += 4;
+            this.vel.add(this.acc);
+            this.pos.add(this.vel);
+            this.acc.mult(0);
+            this.life -= 2;
+            this.color += 4;
         }
     }
 
     isAlive() {
-        return (this.life>0);
+        return (this.life > 0);
     }
 
     applyForce(force) {
@@ -34,8 +34,8 @@ class Particule {
     show() {
         noStroke();
         // const couleur = color('hsba('+this.color+', 100%, 100%,1)');
-        const couleur = color('hsba('+this.color+', 100%, 100%, '+this.life/255+')');
+        const couleur = color('hsba(' + this.color + ', 100%, 100%, ' + this.life / 255 + ')');
         fill(couleur);
-        circle(this.pos.x,this.pos.y,this.r*2);
+        circle(this.pos.x, this.pos.y, this.r * 2);
     }
 }
--- 2_Dev/a003/js/sketch.js
+++ 2_Dev/a003/js/sketch.js
@@ -1,37 +1,39 @@
  let canvas;
 
-let gravity;
-let emetteurs = [];
+ let gravity;
+ let emetteurs = [];
 
-let x,y, r=300, phi;
+ let x, y, r = 300,
+     phi;
 
-function mousePressed() {
-    emetteurs.push(new Emetteur(mouseX,mouseY));
-}
-function setup() {
-    canvas = createCanvas(600, 400);
-    canvas.parent('sketch-XXX');
-    gravity = createVector(0,0.15);
-    colorMode(HSB);
-    phi = -PI/4*3;
-}
+ function mousePressed() {
+     emetteurs.push(new Emetteur(mouseX, mouseY));
+ }
 
-function draw() {
-    background(0);
+ function setup() {
+     canvas = createCanvas(600, 400);
+     canvas.parent('sketch-XXX');
+     gravity = createVector(0, 0.15);
+     colorMode(HSB);
+     phi = -PI / 5;
+ }
 
-    if (phi<=-PI/3 && frameCount%10 === 0) {
-        phi += PI/30;
-        x = r * cos(phi) + width/2;
-        y = r * sin(phi) + height;
-        emetteurs.push(new Emetteur(x,y));
-    }
+ function draw() {
+     background(0);
 
-    for (let e of emetteurs) {
-        if( e.life < 200 && frameCount % 2 === 0) e.emet(floor(e.life/80));
-        e.update();
-        e.show();
-    }
+     if (phi >= -3 * PI / 4 && frameCount % 10 === 0) {
+         phi -= PI / 25;
+         x = r * cos(phi) + width / 2;
+         y = r * sin(phi) + height;
+         emetteurs.push(new Emetteur(x, y));
+     }
 
-    emetteurs = emetteurs.filter(e=> e.isAlive());
+     for (let e of emetteurs) {
+         if (e.life < 200 && frameCount % 12 === 0) e.emet(floor(e.life / 50));
+         e.update();
+         e.show();
+     }
 
-}
+     emetteurs = emetteurs.filter(e => e.isAlive());
+
+ }
